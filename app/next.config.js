b00ec565751d9c288cb7313ca88b4b2a4485d13b
unknown <willyabadquispehuamani@gmail.com>
2023-09-16 14:28:34 -0300

    deploy app

--- app/next.config.js
+++ app/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    API_KEY: process.env.API_KEY || "",
-    API_URL: process.env.API_URL || "",
+    API_KEY: process.env.API_KEY || "957902342",
+    API_URL: process.env.API_URL || "https://api-kallpa.onrender.com",
   },
 };
 
